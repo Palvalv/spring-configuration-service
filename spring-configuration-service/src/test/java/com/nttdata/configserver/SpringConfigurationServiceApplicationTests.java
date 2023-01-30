3426768eb91684e37523633258d3685eaba47ec9
Palvalv <101711875+Palvalv@users.noreply.github.com>
2023-01-30 21:15:00 +0100

    First commit

---
+++ spring-configuration-service/src/test/java/com/nttdata/configserver/SpringConfigurationServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.nttdata.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
